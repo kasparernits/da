4e54e68dd3c76fa27eff717a3ad96227d548d5e3
kasparernits <kaspar.ernits@gmail.com>
2022-05-08 06:33:01 +0200

    add code

--- group_by.py
+++ group_by.py
@@ -1,4 +1,24 @@
-import pandas
+import pandas as pd
 
 print("hello")
+
+dtypes = {
+    "first_name": "category",
+    "gender": "category",
+    "type": "category",
+    "state": "category",
+    "party": "category",
+}
+df = pd.read_csv(
+    "/Users/kasparernits/datasets/groupby-data/legislators-historical.csv",
+    dtype=dtypes,
+    usecols=list(dtypes) + ["birthday", "last_name"],
+    parse_dates=["birthday"]
+)
+
+n_by_state = df.groupby("state")["last_name"].count()
+
+print(n_by_state.head(10))
+
+
 
